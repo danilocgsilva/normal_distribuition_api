cdf0e889004b4e53cf39c4a76b97856ee2f8e900
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-01 11:54:51 -0300

    Adding parameter to ask for amount

--- app/app.py
+++ app/app.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from numpy import random
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def normal_json():
-    ndvalues = random.normal(loc=10000, scale=2000, size=(1,20))[0]
+    ndvalues = get_nd_values(request)
+    
     values = []
     for value in ndvalues:
         values.append(value)
     
     return values
+
+def get_nd_values(request):
+    count_to_generate = 20
+    if request.args.get('count'):
+        count_to_generate = int(request.args.get('count'))
+    
+    ndvalues = results = random.normal(
+        loc=10000, 
+        scale=2000, 
+        size=(1, count_to_generate))
+    
+    return ndvalues[0]
